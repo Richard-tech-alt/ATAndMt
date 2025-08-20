cb745450947322cd3cb9ea8f9fe8adec73c699ad
Richard-tech-alt <richardhawk803@gmail.com>
2025-08-20 08:00:48 -0700

    dcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import ATTSecurityAlert from "./AtScan";
-import InternetScanLayout from "./InternetScanLayout";
-
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<ATTSecurityAlert/>} />
-          <Route path="/scanning" element={<InternetScanLayout/>} />
-      </Routes>
+      <h1>Hello world</h1>
     </div>
   );
 }
